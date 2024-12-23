cd0c5bcb4d7dfef2d587d187fa4a48289684be32
BrunoPolaski <polaskibruno03@gmail.com>
2024-12-23 11:14:07 -0300

    feat: simple concurrent calls to beeceptor just to use waitgroup

--- concurrency/concurrency.go
+++ concurrency/concurrency.go
@@ -2,15 +2,43 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"net/http"
+	"net/url"
 	"sync"
 )
 
 func main() {
 	wg := sync.WaitGroup{}
-	wg.Add(1)
-	go func() {
-		fmt.Println("concurrency")
+	wg.Add(3)
+	request := http.Request{
+		Method: "GET",
+		URL: &url.URL{
+			Host:   "golang.free.beeceptor.com",
+			Scheme: "https",
+		},
+	}
+	requestSender := func() {
+		response, err := http.Get(request.URL.String())
+		if err != nil {
+			panic(err)
+		}
+		defer response.Body.Close()
+
+		responseBody, err := io.ReadAll(response.Body)
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println(string(responseBody), response.StatusCode)
+
 		wg.Done()
-	}()
+	}
+
+	i := 3
+	for i > 0 {
+		go requestSender()
+		i--
+	}
 	wg.Wait()
 }
